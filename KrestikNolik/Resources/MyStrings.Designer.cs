80ae354c0561970bbff26c522af39be63958d20f
Oļegs Miņenkovs <students.minenkovs@macibucentrs.onmicrosoft.com>
2022-11-07 21:08:15 +0200

    fix

--- KrestikNolik/Resources/MyStrings.Designer.cs
+++ KrestikNolik/Resources/MyStrings.Designer.cs
@@ -60,14 +60,18 @@ namespace KrestikNolik.Resources {
             }
         }
         
-
+        /// <summary>
+        ///   Ищет локализованную строку, похожую на Tik Tak Toe.
+        /// </summary>
         public static string MyAppTitle {
             get {
                 return ResourceManager.GetString("MyAppTitle", resourceCulture);
             }
         }
         
-
+        /// <summary>
+        ///   Ищет локализованную строку, похожую на VICTORY! Computer won!.
+        /// </summary>
         public static string MyCompWin {
             get {
                 return ResourceManager.GetString("MyCompWin", resourceCulture);
@@ -128,13 +132,18 @@ namespace KrestikNolik.Resources {
             }
         }
         
+        /// <summary>
+        ///   Ищет локализованную строку, похожую на X-Man VS 0-Computer.
+        /// </summary>
         public static string XmanVSdators {
             get {
                 return ResourceManager.GetString("XmanVSdators", resourceCulture);
             }
         }
         
-
+        /// <summary>
+        ///   Ищет локализованную строку, похожую на X-Man VS 0-Man.
+        /// </summary>
         public static string XmanVSOman {
             get {
                 return ResourceManager.GetString("XmanVSOman", resourceCulture);
